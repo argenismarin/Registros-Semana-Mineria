0d3dc7f1c395e66bcf0c0c65c1f9507c5ac135dc
Argenis Marin <armarina@unal.edu.co>
2025-06-19 09:26:01 -0500

    Cambiar la librería de notificaciones de 'react-hot-toast' a 'react-toastify' en el componente de escarapelas para mejorar la gestión de mensajes al usuario.

--- src/app/escarapelas/page.tsx
+++ src/app/escarapelas/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { toast } from 'react-hot-toast'
+import { toast } from 'react-toastify'
 import Link from 'next/link'
 
 interface Asistente {
